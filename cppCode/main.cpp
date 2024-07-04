8a0c220a6f55a4f8be698e062df48537bb94eb8b
Gabriel Cardoso <94249398+Gabri-el-Batata@users.noreply.github.com>
2024-07-04 12:45:33 -0300

    Add files via upload

--- cppCode/main.cpp
+++ cppCode/main.cpp
@@ -1,97 +1,68 @@
-#include <iostream>
-#include <vector>
-#include "opencv2/highgui.hpp"
-#include "opencv2/stitching.hpp"
+#include <opencv2/opencv.hpp>
 
-using namespace std;
-using namespace cv;
+void stitchImages(const std::vector<cv::Mat> &images, const std::string &diretorio_destino)
+{
+    cv::Ptr<cv::Stitcher> stitcher = cv::Stitcher::create(cv::Stitcher::SCANS);
+    cv::Mat pano;
+    cv::Stitcher::Status status = stitcher->stitch(images, pano);
 
-Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA); // Global Variable
+    stitcher->setPanoConfidenceThresh(0.5); // Ajustar limiar de confiança, se necessário
+    stitcher->setFeaturesFinder(cv::ORB::create());
+    stitcher->setBlender(cv::makePtr<cv::detail::MultiBandBlender>());
 
-bool loadImages(const string& img_path1, const string& img_path2, Mat& img1, Mat& img2) {
-    img1 = imread(img_path1);
-    img2 = imread(img_path2);
+    stitcher->setRegistrationResol(0.6);
+    stitcher->setSeamEstimationResol(0.1);
+    stitcher->setCompositingResol(cv::Stitcher::ORIG_RESOL);
+    stitcher->setWaveCorrection(false);
+    stitcher->setWaveCorrectKind(cv::detail::WAVE_CORRECT_HORIZ);
+    stitcher->setFeaturesFinder(cv::ORB::create());
+    stitcher->setFeaturesMatcher(cv::makePtr<cv::detail::BestOf2NearestMatcher>(true));
+    stitcher->setBundleAdjuster(cv::makePtr<cv::detail::BundleAdjusterRay>());
+    stitcher->setWarper(cv::makePtr<cv::SphericalWarper>());
+    stitcher->setExposureCompensator(cv::makePtr<cv::detail::BlocksGainCompensator>());
+    stitcher->setBlender(cv::makePtr<cv::detail::MultiBandBlender>());
 
-    if(img1.empty() || img2.empty()) {
-        cout << "Error: Some image is empty.\n";
-        return false;
-    }
-
-    return true;
-}
-
-bool stitchImages(const vector<Mat>& imgs, Mat& pano) {
-    Stitcher::Status status = stitcher->stitch(imgs, pano);
-
-    if (status != Stitcher::OK)
+    if (status != cv::Stitcher::OK)
     {
-        cout << "Error: Can't stitch images\n";
-        return false;
+        std::cerr << "Error stitching images, error code: " << status << std::endl;
+        return;
     }
-    return true;
-}
 
-void saveAndDisplay(const Mat &pano, const string& adress) {
     std::stringstream ss;
-    ss<<adress<<"/result.jpg";
+    ss << diretorio_destino <<"/result.png";
     std::string caminho_saida = ss.str();
 
-    imwrite(caminho_saida, pano);
-    imshow("Result", pano);
-    waitKey(0);
-}
+    cv::imwrite(caminho_saida, pano);
 
-void loadConfig(Ptr<Stitcher>& stitcher) {
-    stitcher->setPanoConfidenceThresh(0.8); // Adjust confidence threshold if necessary
-    stitcher->setFeaturesFinder(ORB::create());
-    stitcher->setBlender(makePtr<detail::MultiBandBlender>());
-
-    stitcher->setRegistrationResol(0.5);
-    stitcher->setSeamEstimationResol(0.1);
-    stitcher->setCompositingResol(Stitcher::ORIG_RESOL);
-    stitcher->setWaveCorrection(false);
-    stitcher->setFeaturesFinder(ORB::create());
-    stitcher->setFeaturesMatcher(makePtr<detail::BestOf2NearestMatcher>(false));
-    stitcher->setBundleAdjuster(makePtr<detail::BundleAdjusterRay>());
-    stitcher->setWarper(makePtr<SphericalWarper>());
-    stitcher->setExposureCompensator(makePtr<detail::BlocksGainCompensator>());
-    stitcher->setBlender(makePtr<detail::MultiBandBlender>());
+    cv::imshow("Panorama", pano);
+    cv::waitKey(0);
 }
 
 int main(int argc, char *argv[])
 {
-    // Image paths (consider using a configuration file)
-    if (argc != 4){
-        std::cerr << "Uso: " << argv[0] << "<arg1> <arg2> <arg3>" << std::endl;
-    }
 
-    string img_path1 = argv[1];
-    string img_path2 = argv[2];
-    string diretorio_destino = argv[3];
-
-    // Load images
-    Mat img1, img2;
-    if (!loadImages(img_path1, img_path2, img1, img2))
+    if (argc != 4)
     {
+        std::cerr << "Uso: " << argv[0] << " <arg1> <arg2> <arg3>" << std::endl;
         return -1;
     }
 
-    // Create a vector of images
-    vector<Mat> imgs;
-    imgs.push_back(img1);
-    imgs.push_back(img2);
+    std::string img_path1 = argv[1];
+    std::string img_path2 = argv[2];
+    std::string diretorio_destino = argv[3];
 
-    loadConfig(stitcher);
+    std::vector<cv::Mat> images;
+    images.push_back(cv::imread(img_path1));
+    images.push_back(cv::imread(img_path2));
+    // Add more images as needed
 
-    // Stitch the images
-    Mat pano;
-    if (!(stitchImages(imgs, pano)))
+    if (images.empty())
     {
+        std::cerr << "No images to stitch!" << std::endl;
         return -1;
     }
 
-    // Save and display the stitched image
-    saveAndDisplay(pano, diretorio_destino);
+    stitchImages(images, diretorio_destino);
 
     return 0;
 }
